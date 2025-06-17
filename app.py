b8e02e43da7ec510d2cffcfe5e6446327a8ab4b5
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-17 18:03:23 -0300

    feat: dockerfile created and implemented

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from os import getenv
-
 from flask import Flask
-from flask.cli import load_dotenv
+
+from flask_cors import CORS
 
 from src.route.pdf_csv_routes import pdf_csv
 from src.route.pdf_excel_routes import pdf_excel
 
-load_dotenv()
-
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = getenv("UPLOAD_FOLDER")
+
+resources = {
+    r"*": {
+        "origins": "*"
+    }
+}
+
+CORS(app)
+# CORS(app, resources=resources)
+
+app.register_blueprint(pdf_csv)
+app.register_blueprint(pdf_excel)
 
 
-app.register_blueprint(pdf_csv, pdf_excel)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
